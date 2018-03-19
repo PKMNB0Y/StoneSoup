46e51d3803ea825ebd18ce3e0855435eab787a3b
Unknown <mattiasl380@gmail.com>
2018-03-18 21:10:40 -0400

    Mattias Tiles
    heal, anvil, sand, hole, sand grenade, half wall, shovel

---
+++ Assets/Resources/mll507/scripts/mll507_small_wall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mll507_small_wall : Tile {
+
+	// Walls only take explosive damage.
+	public override void takeDamage(Tile tileDamagingUs, int amount, DamageType damageType) {
+		if (damageType == DamageType.Explosive) {
+			base.takeDamage(tileDamagingUs, amount, damageType);
+		}
+	}
+
+	void Update()
+	{
+		foreach (Tile tile in transform.parent.GetComponentsInChildren<Tile>())
+		{
+			if (tile.hasTag(TileTags.CanBeHeld))
+			{
+				Physics2D.IgnoreCollision(tile.GetComponent<BoxCollider2D>(), _collider, true);
+			}
+		}
+	}
+}
